a9f3c9f48fa104e740b7c0cdd74764955516500d
Ramanpreet Nara <ramanpreet.nara@gmail.com>
2016-09-18 02:24:49 -0400

    feat(api/friendships): Add friendships endpoints

--- src/api/friendships.js
+++ src/api/friendships.js
@@ -1,5 +1,17 @@
+import Boom from 'boom';
+import { decorateWithBoom } from '../util';
 import Friendship from '../model/friendship';
 
-export async function get() {
-
+export async function get(context) {
+  await Friendship.sync();
+  try {
+    context.body = await Friendship.findAll({
+      where: {
+        userId: context.user.id,
+      },
+    });
+  } catch (error) {
+    const boom = Boom.badImplementation('Failed to fetch friendships for logged in user.');
+    decorateWithBoom(boom, context);
+  }
 }
