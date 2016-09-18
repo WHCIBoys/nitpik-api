47e5a0ee53917925968f1686a139a932482c25d4
Ramanpreet Nara <ramanpreet.nara@gmail.com>
2016-09-18 02:27:28 -0400

    feat(api/nit): Add Nit endpoint

--- src/api/nits.js
+++ src/api/nits.js
@@ -1,14 +1,37 @@
 import Nit from '../model/nit';
-import db from '../db';
+import Boom from 'boom';
+import { decorateWithBoom } from '../util/index';
 
-export async function get() {
+export async function get(context) {
+  await Nit.sync();
+  const { query: { authorId, userId } } = context;
 
+  try {
+    const nits = await Nit.findAll({
+      where: { authorId, userId },
+    });
+
+    context.body = nits;
+    context.response.status = 200;
+  } catch (error) {
+    const boom = Boom.badImplementation('Failed to fetch nits');
+    decorateWithBoom(boom, context);
+  }
 }
 
-export async function post() {
-
-}
-
-export async function put() {
-
+export async function post(context) {
+  await Nit.sync();
+  try {
+    const nit = Nit.build(context.body);
+    try {
+      await nit.save();
+    } catch (error) {
+      const boom = Boom.badImplementation('Failed to save Nit to database.', { nit, error });
+      decorateWithBoom(boom, context);
+    }
+  } catch (error) {
+    const message = 'Failed build the Nit.';
+    const boom = Boom.unauthorized(message, error);
+    decorateWithBoom(boom, context);
+  }
 }
