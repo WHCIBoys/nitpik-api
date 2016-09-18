37494d0b109e7ba68494a7044fbbd0899b02eec9
Ramanpreet Nara <ramanpreet.nara@gmail.com>
2016-09-18 05:01:20 -0400

    feat(api/token-grant): Populate friendships from facebook

--- src/api/token-grant.js
+++ src/api/token-grant.js
@@ -1,4 +1,5 @@
 import User from '../model/user';
+import Friendship from '../model/friendship';
 import jsonwebtoken from 'jsonwebtoken';
 import * as S from '../constant/secret';
 import * as Facebook from '../util/facebook';
@@ -11,6 +12,21 @@ export async function post(context) {
 
     await User.sync();
     const [user] = await User.findOrCreate({ where: { facebookId }, defaults: { name } });
+
+    const { data: { data: friends } } = await Facebook.fetchFriends(query.access_token);
+
+    const friendships = friends.map(async (friend) => {
+      const friendUser = await User.findOne({ where: { facebookId: friend.id } });
+      return Friendship.findOrCreate({
+        where: {
+          userId: user.id,
+          friendId: friendUser.id,
+        },
+      });
+    });
+
+    await Promise.all(friendships);
+
     const jwt = jsonwebtoken.sign({}, S.JWT_SECRET, { subject: user.id, expiresIn: '5 days' });
 
     context.body = { jwtToken: jwt, userId: user.id };
