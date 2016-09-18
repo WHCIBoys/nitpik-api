4a63ab16b9b2a9eb33af8446ecb4018fc4efb209
Ramanpreet Nara <ramanpreet.nara@gmail.com>
2016-09-18 01:58:46 -0400

    refactor(middleware/authenticate): Attach logged in user to context

--- src/middleware/authenticate.js
+++ src/middleware/authenticate.js
@@ -1,14 +1,17 @@
 import jsonwebtoken from 'jsonwebtoken';
+import User from '../model/user';
 import Boom from 'boom';
 import * as S from '../constant/secret';
 
 export default async function authenticate(context, next) {
   const token = context.request.header.authorization;
   try {
-    jsonwebtoken.verify(token, S.JWT_SECRET);
+    const decoded = jsonwebtoken.verify(token, S.JWT_SECRET);
+    const { sub: id } = decoded;
+    context.user = await User.findOne({ where: { id } });
     await next();
   } catch (error) {
-    context.body = Boom.unauthorized('Invalid token.');
+    context.body = Boom.unauthorized('Failed to authorize. Token could be invalid.');
     context.response.status = 401;
   }
 }
