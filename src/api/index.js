984ae76600923189afa2c120313f1b4f0d1bf1cb
Ramanpreet Nara <ramanpreet.nara@gmail.com>
2016-09-17 23:27:06 -0400

    Plug in authentication middleware.

--- src/api/index.js
+++ src/api/index.js
@@ -4,17 +4,18 @@ import * as friendships from './friendships';
 import * as nits from './nits';
 import * as users from './users';
 import * as TokenGrant from './token-grant';
+import authenticate from '../middleware/authenticate';
 import db from '../db';
 
 const router = createRouter();
 
-router.get('/friendships', sync(friendships.get));
-router.get('/users/:id', sync(users.get));
-router.post('/users', sync(users.post));
-router.get('/nits', sync(nits.get));
-router.post('/nits', sync(nits.post));
-router.put('/nits/:id', sync(nits.put));
-router.post('/token-grant', sync(TokenGrant.post));
+router.get('/friendships', authenticate, sync(friendships.get));
+router.get('/users/:id', authenticate, sync(users.get));
+router.post('/users', authenticate, sync(users.post));
+router.get('/nits', authenticate, sync(nits.get));
+router.post('/nits', authenticate, sync(nits.post));
+router.put('/nits/:id', authenticate, sync(nits.put));
+router.post('/token-grant', authenticate, sync(TokenGrant.post));
 
 function sync(fn) {
   return async (...args) => {
