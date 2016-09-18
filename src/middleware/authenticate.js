01d3bfda6ac93993d94a710ce9efb35b2617fc1f
Ramanpreet Nara <ramanpreet.nara@gmail.com>
2016-09-18 02:24:16 -0400

    feat(middleware/authenticate): 401 if user not found in db

--- src/middleware/authenticate.js
+++ src/middleware/authenticate.js
@@ -1,6 +1,7 @@
 import jsonwebtoken from 'jsonwebtoken';
 import User from '../model/user';
 import Boom from 'boom';
+import { decorateWithBoom } from '../util';
 import * as S from '../constant/secret';
 
 export default async function authenticate(context, next) {
@@ -9,9 +10,14 @@ export default async function authenticate(context, next) {
     const decoded = jsonwebtoken.verify(token, S.JWT_SECRET);
     const { sub: id } = decoded;
     context.user = await User.findOne({ where: { id } });
+
+    if (context.user === null) {
+      throw new Error('User does not exist.');
+    }
+
     await next();
   } catch (error) {
-    context.body = Boom.unauthorized('Failed to authorize. Token could be invalid.');
-    context.response.status = 401;
+    const boom = Boom.unauthorized(`Failed to authenticate. ${error.message}`);
+    decorateWithBoom(boom, context);
   }
 }
